9e53c24371eacc228fc68a63e0883467aebc682d
Gerry Gleason <ggleason@tribune.com>
2015-01-31 19:36:57 -0600

    Improving special key handling

--- lib/smart_name.rb
+++ lib/smart_name.rb
@@ -4,18 +4,24 @@ require 'active_support/configurable'
 require 'active_support/inflector'
 require 'htmlentities'
 require 'date'
+require 'base64'
 
 class SmartName < Object
   RUBYENCODING = RUBY_VERSION !~ /^1\.8/
   OK4KEY_RE    = RUBYENCODING ? '\p{Word}\*' : '\w\*'
 
   DAYS = (Date::ABBR_DAYNAMES + Date::DAYNAMES).map(&:downcase).to_set
-  MONTHS = (1..12).inject({}) {|h, idx| h[Date::ABBR_MONTHNAMES[idx].downcase] = h[Date::MONTHNAMES[idx].downcase] = idx; h}
+  MONTHS = (1..12).each_with_object({}) do |idx, hash|
+    hash[Date::ABBR_MONTHNAMES[idx].downcase] = hash[Date::MONTHNAMES[idx].downcase] = idx
+  end
+
+  DATE_FORMAT = '%D'
+  DATETIME_FORMAT = '%c'
 
   include ActiveSupport::Configurable
-  
+
   config_accessor :joint, :name_attribute, :banned_array, :var_re, :uninflect, :params, :session
-  
+
   # Wagny defaults:
   #config_accessor :joint,        :default => '+'
   SmartName.joint          = '+'
@@ -45,28 +51,102 @@ class SmartName < Object
     end
 
     def special_keys key
-      if date_parts = key.split('_').reject {|d| DAYS.include? d} and date_parts.length == 3
-        y_idx = date_parts.index {|p| p =~ /^\d{4}$/}
-        if y_idx == 0 or y_idx == 2
-          if y_idx == 0
-            date_parts = date_parts[2], date_parts[1], date_parts[0]
-          else
-            m_idx=nil
-            (0..date_parts.length-1).find do |idx|
-              if month = MONTHS[date_parts[idx].downcase]
-                date_parts[m_idx = idx] = month.to_s
-              end
-            end
-            date_parts[0], date_parts[1] = date_parts[1], date_parts[0] if m_idx == 0 || date_parts[1].to_i > 12
+      key_parts = key.split('_')
+      idx = 0
+      max_idx = key_parts.size - 2
+      while (idx < max_idx) do
+        if DAYS.include?(key_parts[idx])
+          if match_result = date_time_match(key_parts[idx+1..idx+3], key_parts[idx+4..-1])
+            date_time_key, part_count = match_result
+            key_parts[idx..idx+4+part_count] = date_time_key
+            key = key_parts * '_'
           end
-          return "JD#{Date.parse(date_parts*'-').jd}" unless date_parts.find {|p| p=~/\D/}
+          break
+        elsif match_result = date_time_match(key_parts[idx..idx+2], key_parts[idx+3..-1])
+          date_time_key, part_count = match_result
+          key_parts[idx..idx+3+part_count] = date_time_key
+          key = key_parts * '_'
+          break
         end
-      elsif key !~ /\D/
-        return "N#{key.to_i}"
+        idx += 1
+      end
+
+      if key !~ /\D/
+        key = "Q#{Base64.strict_encode64([key.to_i].pack('Q>'))[0..-2]}"
+      elsif key_parts.size > 1 && key_parts.any?{|kp| kp !~ /\D/}
+        key = key_parts.map{|kp| kp =~ /\D/ ? kp : "L#{Base64.strict_encode64([kp.to_i].pack('L>'))[0..-3]}"}*'_'
       end
       key
+    end
+
+    def date_time_match date_parts, time_parts
+      y_idx = date_parts.index {|p| p =~ /^\d{4}$/}
+      if [0, 2].include? y_idx
+        if y_idx == 2
+          date_parts = date_parts[2], date_parts[1], date_parts[0]
+          m_idx=nil
+          (0..date_parts.length-1).find do |idx|
+            if month = MONTHS[date_parts[idx].downcase]
+              date_parts[m_idx = idx] = month.to_s
+            end
+          end
+          date_parts[2], date_parts[1] = date_parts[1], date_parts[2] if m_idx == 0 || date_parts[1].to_i > 12
+        end
+
+        num_time_parts = 0
+        if time_parts.first =~ /^([ap]m)?(\d{1,2})([ap]m)?$/i
+          min = sec = 0
+          h = $2.to_i
+          if $3 # HH[AP]M
+            num_time_parts, hour, am_pm = 1, h, $3 unless $1 || h > 12
+          elsif $1 && h <= 12 || h <= 23 # [[AP]M]HH[:MM:SS]
+            hour, am_pm = h, $1
+          end
+          if hour
+            if num_time_parts == 0
+              if time_parts[1]
+                if time_parts[1] =~ /^(\d{1,2})([ap]m)?$/i && $1.to_i < 60
+                  if $2 # HH:MM[AP]M
+                    num_time_parts, min, am_pm = 2, $1.to_i, $2 unless am_pm
+                  else # [[AP]M]HH:MM[:SS]
+                    min = $1.to_i
+                  end
+                  if time_parts[2]
+                    if time_parts[2] =~ /^(\d{1,2})([ap]m)?$/i && $1.to_i < 60
+                      am_pm_new, s = $2, $1.to_i
+                      if !am_pm_new && time_parts[3] && time_parts[3] =~ /^[ap]m$/i
+                        num_time_parts, sec, am_pm = 4, s, time_parts[3]
+                      elsif !(am_pm_new && am_pm)
+                        num_time_parts, sec, am_pm = 3, $1.to_i, am_pm_new
+                      end
+                    elsif !am_pm && time_parts[2] =~ /^[ap]m$/i
+                      num_time_parts, ap_pm = 3, time_parts[2]
+                    end
+                  end
+                elsif !am_pm && time_parts[1] =~ /^[ap]m$/i
+                  num_time_parts, ap_pm = 2, time_parts[1]
+                end
+              end
+            end
+            if am_pm
+              hour = 0 if hour == 12
+              hour += 12 if ['p', 'P'].include? am_pm[0]
+            end
+          end
+          hms = [hour, min, sec]
+        end
+        unless date_parts.any? {|p| p=~/\D/}
+          hms ||= [0, 0, 0]
+          time = Time.new(*(date_parts.map(&:to_i)), *hms)
+          if num_time_parts > 0
+            ["T#{Base64.strict_encode64([time.to_i].pack('Q>'))[0..-2]}", num_time_parts]
+          else
+            ["D#{Base64.strict_encode64([time.to_date.jd].pack('L>'))[0..-3]}", 0]
+          end
+        end
+      end
     rescue ArgumentError => e
-      key
+      nil
     end
   end
 
@@ -101,7 +181,7 @@ class SmartName < Object
 
   def valid?
     not parts.find do |pt|
-      pt.match self.class.banned_re 
+      pt.match self.class.banned_re
     end
   end
 
@@ -137,6 +217,31 @@ class SmartName < Object
     @decoded ||= (s.index('&') ?  HTMLEntities.new.decode(s) : s)
   end
 
+  def pretty_key
+    parts.map do |part|
+      part.to_name.key.split('_').map do |kp|
+        if kp =~ /^[A-Z]/ && num_string = Base64.decode64($')
+          case $&
+            when 'D'
+              j = num_string.unpack('L>').first
+              "Date: #{Date.jd(j).strftime(DATE_FORMAT)}"
+            when 'T'
+              j = num_string.unpack('Q>').first
+              "Datetime: #{Time.at(j).strftime(DATETIME_FORMAT)}"
+            when 'L'
+              j = num_string.unpack('L>').first
+              "Long: #{j}"
+            when 'Q'
+              j = num_string.unpack('Q>').first
+              "Long Long: #{j}"
+          end
+        else
+          kp
+        end
+      end * '::'
+    end * " #{self.class.joint} "
+  end
+
   def pre_cgi
     #why is this necessary?? doesn't real CGI escaping handle this??
     # hmmm.  is this to prevent absolutizing
@@ -155,21 +260,21 @@ class SmartName < Object
 
   def left()        @left  ||= simple? ? nil : parts[0..-2]*self.class.joint end
   def right()       @right ||= simple? ? nil : parts[-1]                    end
-                    
+
   def left_name()   @left_name   ||= left  && self.class.new( left  )        end
   def right_name()  @right_name  ||= right && self.class.new( right )        end
-                    
+
   # Note that all n ames have a trunk and tag, but only junctions have left and right
-                    
+
   def trunk()       @trunk ||= simple? ? s : left                           end
   def tag()         @tag   ||= simple? ? s : right                          end
-                    
+
   def trunk_name()  @trunk_name  ||= simple? ? self : left_name             end
   def tag_name()    @tag_name    ||= simple? ? self : right_name            end
-                                                                            
+
   def part_names()  @part_names  ||= parts.map  &:to_name                   end
   def piece_names() @piece_names ||= pieces.map &:to_name                   end
-                                                                            
+
   def pieces
     @pieces ||= if simple?
       [ self ]
@@ -197,7 +302,7 @@ class SmartName < Object
     end
 
     show_name = show_parts.compact.to_name.s
-    
+
     case
     when show_parts.compact.empty?;  self
     when show_parts[0].nil?       ;  self.class.joint + show_name
@@ -275,7 +380,7 @@ class SmartName < Object
   # shouldn't it use inclusions???
   def self.substitute! str, hash
     hash.keys.each do |var|
-      str.gsub! var_re do |x| 
+      str.gsub! var_re do |x|
         hash[var.to_sym]
       end
     end
--- spec/lib/date_name_spec.rb
+++ spec/lib/date_name_spec.rb
@@ -5,33 +5,99 @@ require 'core_ext'
 
 describe SmartName do
 
-  describe "#key" do
-    it "should remove spaces" do
-      "January 1, 1999".to_name.key.should == "JD2451180"
+  describe '#key' do
+    it 'should remove spaces and non-key chars' do
+      n=' January -_& 1, 1999'.to_name
+      n.pretty_key.should == 'Date: 01/01/99'
+      n.key.should == 'DACVm7A'
     end
 
-    it "should do Y M D" do
-      "1999-1-31".to_name.key.should == "JD2451210"
+    it 'should do Y M D' do
+      n='1999-1-31'.to_name
+      n.pretty_key.should == 'Date: 01/31/99'
+      n.key.should == 'DACVnCg'
     end
 
-    it "should not do Y D M" do
-      "1999-31-1".to_name.key.should == "1999_31_1"
+    it 'should not do Y D M' do
+      n='1999-31-1'.to_name
+      n.pretty_key.should == 'Long: 1999::Long: 31::Long: 1'
+      n.key.should == 'LAAAHzw_LAAAAHw_LAAAAAQ'
     end
 
-    it "should do M D Y based on range" do
-      "1 31 1999".to_name.key.should == "JD2451210"
+    it 'should do M D Y based on range' do
+      n='1 31 1999'.to_name
+      n.pretty_key.should == 'Date: 01/31/99'
+      n.key.should == 'DACVnCg'
     end
 
-    it "should default to D M Y" do
-      "1 10 1999".to_name.key.should == "JD2451453"
+    it 'should default to D M Y' do
+      n='1 10 1999'.to_name
+      n.pretty_key.should == 'Date: 10/01/99'
+      n.key.should == 'DACVn/Q'
     end
 
-    it "should ignore week day" do
-      "Friday, Jan 10, 1999".to_name.key.should == "JD2451189"
+    it 'should ignore week day' do
+      n='Friday, Jan 10, 1999'.to_name
+      n.pretty_key.should == 'Date: 10/01/99'
+      n.key.should == 'DACVn/Q'
     end
 
-    it "should not do year in the middle" do
-      "Jan 1999 10".to_name.key.should == "jan_1999_10"
+    it 'should not do year in the middle' do
+      n='Jan 1999 10'.to_name
+      n.pretty_key.should == 'jan::Long: 1999::Long: 10'
+      n.key.should == 'jan_LAAAHzw_LAAAACg'
+    end
+
+    it 'should not match unless right after ignored week day' do
+      n='Friday, on Jan 10, 1999'.to_name
+      n.pretty_key.should == 'friday::on::jan::Long: 10::Long: 1999'
+      n.key.should == 'friday_on_jan_LAAAACg_LAAAHzw'
+    end
+
+    it 'matches after other keys' do
+      n='some, key Jan 10, 1999'.to_name
+      n.pretty_key.should == 'some::key::Date: 10/01/99'
+      n.key.should == 'some_key_DACVn/Q'
+    end
+
+    it 'matches after number keys' do
+      n='10 20 30, Jan 10, 1999'.to_name
+      n.pretty_key.should == 'Long: 10::Long: 20::Long: 30::Date: 10/01/99'
+      n.key.should == 'LAAAACg_LAAAAFA_LAAAAHg_DACVn/Q'
+    end
+
+    it 'matches after number keys, including time' do
+      n='10 20 30, Jan 10, 1999 10am'.to_name
+      n.pretty_key.should == 'Long: 10::Long: 20::Long: 30::Datetime: Fri Oct  1 10:00:00 1999'
+      n.key.should == 'LAAAACg_LAAAAFA_LAAAAHg_TAAAAADf0zHA'
+    end
+
+    it 'matches after number keys, including time' do
+      n='10 20 30, Jan 10, 1999 10AM'.to_name
+      n.pretty_key.should == 'Long: 10::Long: 20::Long: 30::Datetime: Fri Oct  1 10:00:00 1999'
+      n.key.should == 'LAAAACg_LAAAAFA_LAAAAHg_TAAAAADf0zHA'
+    end
+
+    it 'matches include 24 hour time' do
+      n='Jan 10, 1999 22:00:00'.to_name
+      n.pretty_key.should == 'Datetime: Fri Oct  1 22:00:00 1999'
+      n.key.should == 'TAAAAADf1dTA'
+    end
+
+    it 'matches after number keys' do
+      n='10 20 30, Jan 10, 1999'.to_name
+      n.pretty_key.should == 'Long: 10::Long: 20::Long: 30::Date: 10/01/99'
+      n.key.should == 'LAAAACg_LAAAAFA_LAAAAHg_DACVn/Q'
+    end
+
+    it 'matches number' do
+      n= '0000012345678901234'.to_name
+      n1= '   12345678901234'.to_name
+      n2= '12345678901234'.to_name
+      n.key.should == n1.key
+      n.key.should == n2.key
+      n.pretty_key.should == 'Long Long: 12345678901234'
+      n.key.should == 'QAAALOnPOL/I'
     end
   end
 end
